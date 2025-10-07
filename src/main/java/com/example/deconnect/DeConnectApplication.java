1fefb7fec8ecebecc51c7c02301aa259774062c0
Dávid Szűcs <szucsd65@gmail.com>
2025-10-07 13:40:12 +0200

    Initial Commit

---
+++ src/main/java/com/example/deconnect/DeConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.deconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deconnect/DeConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
